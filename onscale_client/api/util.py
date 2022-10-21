f0546961ebf2877107230759c5188a462739f78e
Brock Smith <brock.smith@onscale.com>
2022-10-21 13:10:32 -0400

    Fixed cloud client to work with current mesh generation

--- onscale_client/api/util.py
+++ onscale_client/api/util.py
@@ -7,6 +7,33 @@ from onscale_client.api.datamodel import BlobType
 from onscale_client.api.rest_api import rest_api
 
 
+def wait_for_blob(blob_type: str,
+                  object_id: str,
+                  object_type: str,
+                  timeout_secs: float = None):
+    """ Wait for a blob to exist of a specific type polling with backoff
+    """
+
+    def poll():
+        """ Inner poll function to be retried """
+        response = rest_api.blob_list_object(blob_type=blob_type,
+                                             object_id=object_id,
+                                             object_type=object_type)
+        return len(response) > 0
+
+    now = time.time()
+    retry_secs = 2
+    while True:
+        if timeout_secs is not None and (time.time() - now) > timeout_secs:
+            raise TimeoutError(f"No blob of {object_id} of type "
+                               f"{blob_type} found")
+        if poll():
+            break
+        time.sleep(retry_secs)
+        # Exponential backoff, maximum 30 seconds
+        retry_secs = min(retry_secs * 2, 30)
+
+
 def wait_for_child_blob(
     parent_blob_id: str, blob_type: str, timeout_secs: float = None
 ):
