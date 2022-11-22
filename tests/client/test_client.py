26789ba0c6a94e170227cb6db7049ff4edf01b7b
Cyprien Rusu <cyprien.rusu@onscale.com>
2022-11-22 14:35:00 +0100

    adding new test

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -9,5 +9,22 @@ def client():
 
 # Test that the client is created and contains at least 1 account_id
 def test_account_ids_contains_id(client):
-
     assert len(client.account_ids())>1
+
+# Test that current account id corresponds to the first account in the list
+def test_current_account_id(client):
+
+    assert client.current_account_id == client.account_ids()[0]
+
+# Test to set the account to OnScale US
+def test_set_account(client):
+    
+    # Check that account OnScale US exists and get its id
+    client.account_exists('OnScale US')
+    account_id = client.account_id('OnScale US')
+    
+    # Set the current account to OnScale US
+    client.set_current_account('OnScale US')
+    
+    # Check that the current account set has the correct id
+    assert client.current_account_id == account_id
