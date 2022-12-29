9b70a74f95ce58dbd2ddbc3502904fdb1d81668a
Jeremy Theler <jeremy.theler@onscale.com>
2022-12-29 15:12:49 -0300

    estimate attached to version not to study

--- onscale_client/branch.py
+++ onscale_client/branch.py
@@ -22,108 +22,135 @@ from onscale_client.sockets.estimate_listener import EstimateListener
 import pdb
 
 
-class Study:
+# class Study:
+#     def __init__(
+#         self,
+#         id: str,
+#         simapi_blob_id: str
+#     ):
+#         self.__data: datamodel.Job = RestApi.job_load(id)
+#         self.estimate_listener: EstimateListener
+# 
+#         self.job_id: str = id
+#         self.account_id: str = self.__data.account_id
+#         self.parent_job_id: str = self.__data.parent_job_id
+#         self.project_id: str = self.__data.project_id
+#         self.design_id: str = self.__data.design_id
+#         self.design_instance_id: str = self.__data.design_instance_id
+#         
+#         self.simapi_blob_id = simapi_blob_id
+# 
+#     @property
+#     def id(self) -> str:
+#         return self.__data.job_id
+# 
+# 
+#     def __str__(self):
+#         """string representation of Study object"""
+#         return_str = "Study(\n"
+#         return_str += f"    job_id={self.__data.job_id},\n"
+#         return_str += f"    account_id={self.__data.account_id},\n"
+#         return_str += f"    job_name={self.__data.job_name},\n"
+#         return_str += f"    cores_required={self.__data.cores_required},\n"
+#         return_str += f"    ram_estimate={self.__data.ram_estimate},\n"
+#         return_str += f"    main_file={self.__data.main_file},\n"
+#         return_str += f"    precision={self.__data.precision},\n"
+#         return_str += f"    number_of_parts={self.__data.number_of_parts},\n"
+#         return_str += f"    docker_tag={self.__data.docker_tag},\n"
+#         return_str += f"    docker_tag_id={self.__data.docker_tag_id},\n"
+#         return_str += f"    file_dependencies={self.__data.file_dependencies},\n"
+#         return_str += f"    file_aliases={self.__data.file_aliases},\n"
+#         return_str += f"    operation={self.__data.operation},\n"
+#         return_str += f"    preprocessor={self.__data.preprocessor},\n"
+#         return_str += f"    simulations={self.__data.simulations},\n"
+#         return_str += f"    file_aliases={self.__data.file_aliases},\n"
+#         return_str += f"    job_status={self.__data.job_status},\n"
+#         return_str += f"    job_type={self.__data.job_type},\n"
+#         return_str += f"    log_group={self.__data.log_group},\n"
+#         return_str += f"    hpc_id={self.__data.hpc_id},\n"
+#         return_str += f"    application={self.__data.application},\n"
+#         return_str += f"    parent_job_id={self.__data.parent_job_id},\n"
+#         return_str += f"    parent_job_id={self.__data.parent_job_id},\n"
+#         return_str += f"    project_id={self.__data.project_id},\n"
+#         return_str += f"    design_id={self.__data.design_id},\n"
+#         return_str += f"    design_instance_id={self.__data.design_instance_id},\n"
+#         return_str += f"    supervisor_id={self.__data.supervisor_id},\n"
+#         return_str += f"    time_to_end={self.__data.time_to_end},\n"
+#         return_str += f"    max_spend_ch={self.__data.max_spend_ch},\n"
+#         return_str += f"    job_cost={self.__data.job_cost},\n"
+#         return_str += f"    user_id={self.__data.user_id},\n"
+#         return_str += f"    last_status={self.__data.last_status},\n"
+#         return_str += f"    last_status_date={self.__data.last_status_date},\n"
+#         return_str += f"    final_status={self.__data.final_status},\n"
+#         return_str += f"    queued_status_date={self.__data.queued_status_date},\n"
+#         return_str += f"    deleted_date={self.__data.deleted_date},\n"
+#         return_str += f"    physics_types={self.__data.physics_types},\n"
+#         return_str += f"    console_parameter_names={self.__data.console_parameter_names},\n"
+#         return_str += f"    design_title={self.__data.design_title},\n"
+#         return_str += f"    design_instance_title={self.__data.design_instance_title},\n"
+#         return_str += f"    file_dependent_job_id_list={self.__data.file_dependent_job_id_list},\n"
+#         return_str += ")"
+#         return return_str
+
+class Estimate:
     def __init__(
         self,
-        id: str,
-        simapi_blob_id: str
+        estimate_id: str,
+        type: str,
+        number_of_cores: List[int],
+        estimated_memory: List[int],
+        estimated_run_times: List[int],
+        parts_count: List[int],
+        parameters: str,
+        estimate_hashes: List[str],
+        wallclock: List[float],
+        ch: List[float],
+        hpc_id: str,
+        stage: str,
+        account_id: str,
+        job_id: str,
+        user_id: str
     ):
-        self.__data: datamodel.Job = RestApi.job_load(id)
-        self.estimate_listener: EstimateListener
 
-        self.job_id: str = id
-        self.account_id: str = self.__data.account_id
-        self.parent_job_id: str = self.__data.parent_job_id
-        self.project_id: str = self.__data.project_id
-        self.design_id: str = self.__data.design_id
-        self.design_instance_id: str = self.__data.design_instance_id
-        
-        self.simapi_blob_id = simapi_blob_id
+        self.estimate_id = estimate_id
+        self.number_of_cores = number_of_cores
+        self.estimated_memory = estimated_memory
+        self.estimated_run_times = estimated_run_times
+        self.parts_count = parts_count
+        self.parameters = parameters
+        self.estimate_hashes = estimate_hashes
+        self.wallclock = wallclock
+        self.ch = ch
+        self.hpc_id = hpc_id
+        self.stage = stage
+        self.account_id = account_id
+        self.job_id = job_id
+        self.user_id = user_id
 
     @property
     def id(self) -> str:
-        return self.__data.job_id
-
+        return self.estimate_id
 
     def __str__(self):
-        """string representation of Study object"""
-        return_str = "Study(\n"
-        return_str += f"    job_id={self.__data.job_id},\n"
-        return_str += f"    account_id={self.__data.account_id},\n"
-        return_str += f"    job_name={self.__data.job_name},\n"
-        return_str += f"    cores_required={self.__data.cores_required},\n"
-        return_str += f"    ram_estimate={self.__data.ram_estimate},\n"
-        return_str += f"    main_file={self.__data.main_file},\n"
-        return_str += f"    precision={self.__data.precision},\n"
-        return_str += f"    number_of_parts={self.__data.number_of_parts},\n"
-        return_str += f"    docker_tag={self.__data.docker_tag},\n"
-        return_str += f"    docker_tag_id={self.__data.docker_tag_id},\n"
-        return_str += f"    file_dependencies={self.__data.file_dependencies},\n"
-        return_str += f"    file_aliases={self.__data.file_aliases},\n"
-        return_str += f"    operation={self.__data.operation},\n"
-        return_str += f"    preprocessor={self.__data.preprocessor},\n"
-        return_str += f"    simulations={self.__data.simulations},\n"
-        return_str += f"    file_aliases={self.__data.file_aliases},\n"
-        return_str += f"    job_status={self.__data.job_status},\n"
-        return_str += f"    job_type={self.__data.job_type},\n"
-        return_str += f"    log_group={self.__data.log_group},\n"
-        return_str += f"    hpc_id={self.__data.hpc_id},\n"
-        return_str += f"    application={self.__data.application},\n"
-        return_str += f"    parent_job_id={self.__data.parent_job_id},\n"
-        return_str += f"    parent_job_id={self.__data.parent_job_id},\n"
-        return_str += f"    project_id={self.__data.project_id},\n"
-        return_str += f"    design_id={self.__data.design_id},\n"
-        return_str += f"    design_instance_id={self.__data.design_instance_id},\n"
-        return_str += f"    supervisor_id={self.__data.supervisor_id},\n"
-        return_str += f"    time_to_end={self.__data.time_to_end},\n"
-        return_str += f"    max_spend_ch={self.__data.max_spend_ch},\n"
-        return_str += f"    job_cost={self.__data.job_cost},\n"
-        return_str += f"    user_id={self.__data.user_id},\n"
-        return_str += f"    last_status={self.__data.last_status},\n"
-        return_str += f"    last_status_date={self.__data.last_status_date},\n"
-        return_str += f"    final_status={self.__data.final_status},\n"
-        return_str += f"    queued_status_date={self.__data.queued_status_date},\n"
-        return_str += f"    deleted_date={self.__data.deleted_date},\n"
-        return_str += f"    physics_types={self.__data.physics_types},\n"
-        return_str += f"    console_parameter_names={self.__data.console_parameter_names},\n"
-        return_str += f"    design_title={self.__data.design_title},\n"
-        return_str += f"    design_instance_title={self.__data.design_instance_title},\n"
-        return_str += f"    file_dependent_job_id_list={self.__data.file_dependent_job_id_list},\n"
+        """string representation of Estimate object"""
+        return_str = "Estimate(\n"
+        return_str += f"    estimate_id={self.estimate_id},\n"
+        return_str += f"    number_of_cores={self.number_of_cores},\n"
+        return_str += f"    estimated_memory={self.estimated_memory},\n"
+        return_str += f"    estimated_run_times={self.estimated_run_times},\n"
+        return_str += f"    parts_count={self.parts_count},\n"
+        return_str += f"    parameters={self.parameters},\n"
+        return_str += f"    estimate_hashes={self.estimate_hashes},\n"
+        return_str += f"    wallclock={self.wallclock},\n"
+        return_str += f"    ch={self.ch},\n"
+        return_str += f"    hpc_id={self.hpc_id},\n"
+        return_str += f"    stage={self.stage},\n"
+        return_str += f"    account_id={self.account_id},\n"
+        return_str += f"    job_id={self.job_id},\n"
+        return_str += f"    user_id={self.user_id},\n"
         return_str += ")"
         return return_str
 
-    def estimate(self, portal: str, token: str) -> EstimateListener:
-        
-        if self.simapi_blob_id == None:
-            self.simapi_blob_id = self.getSimapiBlob()
-            
-        if self.simapi_blob_id == None:
-            print("error: version %s does not have simapi blob so it cannot be estimated", self.design_instance_id)
-            return None
-        
-        try:
-            response = RestApi.job_estimate(
-                job_id=self.__data.job_id,
-                solver="REFLEX",
-                blob_id=self.simapi_blob_id
-            )
-            self.estimate_id = response.estimate_id;
-            print("estimating: study_id = %s, estimate_id = %s" % (self.job_id ,self.estimate_id))
-    
-            # TODO: how come this guy does not need the estimate id?
-            self.estimate_listener = EstimateListener(portal=portal, token=token)
-            self.estimate_listener.listen(timeout_secs=3*60)
-
-        except TimeoutError:
-            print("Timed out waiting for estimate")
-            self.estimate_listener.error = 1
-        except rest_api.ApiError as e:
-            print(f"ApiError raised - {str(e)}")
-            self.estimate_listener.error = 2
-            
-        return self.estimate_listener
-        
-        
 
 class Version:
     def __init__(
@@ -143,6 +170,9 @@ class Version:
         self.design_instance_id: str = id
         self.project_id: str = self.__data.project_id
         self.design_id: str = self.__data.design_id
+        self.job_id: str = None
+        
+        self.estimate_listener: EstimateListener
         
         if hpc_id != None and account_id != None:
             self.hpc_id = hpc_id
@@ -174,13 +204,6 @@ class Version:
         return_str += ")"
         return return_str
 
-    def listStudies(self) -> dict:
-        # re-load design instance to update the list of analysis
-        pdb.set_trace()
-        response = RestApi.design_instance_load(self.__data.design_instance_id)
-        self.__data.analysis_list = response.analysis_list
-        return self.__data.analysis_list
-
     def listBlobs(self) -> dict:
         try:
             response = RestApi.blob_list(object_id = self.__data.design_instance_id)
@@ -194,15 +217,18 @@ class Version:
             print("error: cannot find path '%s'" % cad_file_name)
 
         cad_md5 = hash_file(cad_file_name)
-        self.cad_file_name = cad_file_name
 
         cad_blob_id = None
         mesh_file_name = None
 
         for blob in self.listBlobs():
-            if cad_blob_id is None and blob.blob_type == datamodel.BlobType.CAD and cad_md5 == blob.hash:
-                print("CAD file %s is already in the version (hash %s)" % (cad_file_name, cad_md5))
-                cad_blob_id = blob.blob_id
+            if cad_blob_id is None and blob.blob_type == datamodel.BlobType.CAD:
+                if cad_md5 == blob.hash:
+                    print("CAD file %s is already in the version (hash %s)" % (cad_file_name, cad_md5))
+                    cad_blob_id = blob.blob_id
+                else:
+                    print("version already has a CAD file which is different from the one being added %s, not adding anything" % cad_file_name)
+                    return None
             elif mesh_file_name is None and blob.blob_type == datamodel.BlobType.MESHAUTO:
                 # TODO: store the file name in a dictionary CAD hash -> mesh name
                 print("CAD file already has an automatic mesh (%s)" % (blob.original_file_name))
@@ -220,6 +246,7 @@ class Version:
             print(response)
             cad_blob_id = response.blob_id
 
+        self.cad_file_name = cad_file_name
         self.cad_blob_id = cad_blob_id
         return cad_blob_id
 
@@ -317,16 +344,18 @@ class Version:
         
                 
     def addSimapiBlob(self, simapi_file_path: str) -> str:
-        # TODO: allow only one blob per version
         self.updateMeshName(simapi_file_path)
-        
         simapi_file_md5 = hash_file(simapi_file_path)
         response = RestApi.blob_list(object_id = self.__data.design_instance_id);
         simapi_blob_id = None
         for blob in response:
-            if blob.blob_type == datamodel.BlobType.SIMAPI and simapi_file_md5 == blob.hash:
-                simapi_blob_id = blob.blob_id
-                print("SimAPI file is already in the project (hash %s)" % (simapi_file_md5))
+            if blob.blob_type == datamodel.BlobType.SIMAPI:
+                if simapi_file_md5 == blob.hash:
+                    simapi_blob_id = blob.blob_id
+                    print("SimAPI file is already in the version (hash %s)" % (simapi_file_md5))
+                else:
+                    print("The version already has a SimAPI file which is different from the one being added, not adding anything")
+                    return None
 
         if simapi_blob_id is None:
             print("SimAPI file is not in the project, uploading...")
@@ -337,38 +366,61 @@ class Version:
                 file = simapi_file_path)
             simapi_blob_id = response.blob_id
 
+
         # now we need to add the metadata (TODO: remove the need to)
         self.addSimMetadata(simapi_blob_id, simapi_file_md5)
         self.simapi_blob_id = simapi_blob_id
 
         return simapi_blob_id
         
-    def getStudy(self, id: Optional[str] = None) -> Study:
-        """Load a study object from a version.
+    
 
-        Args:
-            id: the id of the loaded study. If empty, the last study is returned.
-        """
-
-        if id == None:
-            # we need to re-load the branch object to have a fresh list of versions
-            studies = self.listStudies()
-            id = studies[len(studies)-1].analysis_id
-
-        study = Study(id, self.simapi_blob_id)
-        if study.design_instance_id != self.__data.design_instance_id:
-            print("study %s does not belong to version %s" % (id, self.__data.design_instance_id))
-
-        return study
+    def estimate(self, portal: str, token: str) -> Estimate:
         
+        if self.simapi_blob_id == None:
+            self.simapi_blob_id = self.getSimapiBlob()
+            
+        if self.simapi_blob_id == None:
+            print("error: version %s does not have simapi blob so it cannot be estimated", self.design_instance_id)
+            return None
         
-    def createStudy(self) -> Study:
+        self.job_id = RestApi.job_init(account_id=self.account_id, hpc_id=self.hpc_id)    
+        
         try:
-            study_id = RestApi.job_init(account_id=self.account_id, hpc_id=self.hpc_id)
-        except rest_api.ApiError as e:
-            print(f"APIError raised - {str(e)}")
+            response = RestApi.job_estimate(
+                job_id=self.job_id,
+                solver="REFLEX",
+                blob_id=self.simapi_blob_id
+            )
+            self.estimate_id = response.estimate_id;
+            print("estimating: study_id = %s, estimate_id = %s" % (self.job_id ,self.estimate_id))
+    
+            # TODO: how come this guy does not need the estimate id?
+            self.estimate_listener = EstimateListener(portal=portal, token=token)
+            self.estimate_listener.listen(timeout_secs=3*60)
 
-        return Study(study_id, self.simapi_blob_id)
+        except TimeoutError:
+            print("Timed out waiting for estimate")
+            return None
+        except rest_api.ApiError as e:
+            print(f"ApiError raised - {str(e)}")
+            return None
+            
+        return Estimate(type = self.estimate_listener.type,
+            number_of_cores = self.estimate_listener.number_of_cores,
+            estimated_memory = self.estimate_listener.estimated_memory,
+            estimated_run_times = self.estimate_listener.estimated_run_times,
+            parts_count = self.estimate_listener.parts_count,
+            parameters = self.estimate_listener.parameters,
+            estimate_hashes = self.estimate_listener.estimate_hashes,
+            wallclock = self.estimate_listener.wallclock,
+            ch = self.estimate_listener.ch,
+            hpc_id = self.estimate_listener.hpc_id,
+            stage = self.estimate_listener.stage,
+            account_id = self.estimate_listener.account_id,
+            job_id = self.estimate_listener.job_id,
+            estimate_id = self.estimate_listener.estimate_id,
+            user_id = self.estimate_listener.user_id)
 
 
 class Branch(object):
